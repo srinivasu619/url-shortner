9ae30dfd6f585c34a88af78cb7ed478a92ef78af
srinivasu619 <srinivasu.618@gmail.com>
2019-12-24 16:01:33 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const {apiRoutes} = require("./routes");
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set("view engine", "pug");
+app.use('/api/url', apiRoutes);
+app.get("/", function(req, res) {
+  res.render("index", { title: "Hey", message: "Hello there!" });
+});
+
+app.listen(3000);
