513d937d19a13343299fccb258a58ec717df8aae
srinivasu619 <srinivasu.618@gmail.com>
2019-12-26 14:02:30 +0530

    Added Routes for the API

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const dns = require("dns");
 const randomstring = require("randomstring");
+const { insertRecord, fecthRecord } = require("../db/db-client");
+const { urlValidator } = require("../validator");
 var router = express.Router();
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const url = req.body.url;
-  dns.lookup(url, (err, address, family) => {
-    if (err)
-      return res.send({
-        error: "Invalid URL"
-      });
-    else {
-      let id = randomstring.generate(7);
-      return res.send({
-        id,
-        original_url: url
-      });
-    }
-  });
+router.post("/", urlValidator, (req, res) => {
+  let data = {
+    orignalURL: req.body.url,
+    code: randomstring.generate(7)
+  };
+  try {
+    insertRecord(data, function(result) {
+      return res.status(201).send(result);
+    });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  let data = {
+    code: req.params.id
+  };
+  try {
+    fecthRecord(data, function(result) {
+      return res.status(302).redirect(result["orignalURL"]);
+    });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
 
 module.exports = router;
